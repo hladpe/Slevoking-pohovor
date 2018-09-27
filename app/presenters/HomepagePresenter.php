b8121bce87edfee2d86202ac8a1fbd969ea08fc3
Petr Hladík <hladpe@gmail.com>
2018-09-27 20:09:02 +0200

    Project setup

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+class HomepagePresenter extends Presenter
+{
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Slevoking - Vstupní test
+========================
+
+Realizace zadání z pohovoru na pozici PHP programátora.
